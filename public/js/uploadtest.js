1e48d561b94adacaea9a17cb256100f34b59c920
ChrisMelfi <chris.melfi@hotmail.co.uk>
2017-04-17 04:33:40 +0100

    urgh

--- public/js/uploadtest.js
+++ public/js/uploadtest.js
@@ -1,17 +1,46 @@
-function uploadFile (facialanalysisbucket, test1.png) {
+/**
+ * Copyright 2016, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This application demonstrates how to perform basic operations on files with
+ * the Google Cloud Storage API.
+ *
+ * For more information, see the README.md under /storage and the documentation
+ * at https://cloud.google.com/storage/docs.
+ */
+
+'use strict';
+
+const Storage = require('@google-cloud/storage');
+
+// [START storage_list_files]
+function listFiles (bucketName) {
   // Instantiates a client
   const storage = Storage();
 
   // References an existing bucket, e.g. "my-bucket"
-  const bucket = storage.bucket('facialanalysisbucket');
+  const bucket = storage.bucket(bucketName);
 
-  // Uploads a local file to the bucket, e.g. "./local/path/to/file.txt"
-  return bucket.upload('test1.png')
+  // Lists files in the bucket
+  return bucket.getFiles()
     .then((results) => {
-      const file = results[0];
+      const files = results[0];
 
-      console.log(`File ${file.name} uploaded.`);
+      console.log('Files:');
+      files.forEach((file) => console.log(file.name));
 
-      return file;
+      return files;
     });
 }
