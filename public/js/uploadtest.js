a3edeb2672ddd3cb22a81249dc7c3957dd3667e9
ChrisMelfi <chris.melfi@hotmail.co.uk>
2017-04-17 03:12:10 +0100

    storage test

---
+++ public/js/uploadtest.js
@@ -0,0 +1,17 @@
+function uploadFile (bucketName, fileName) {
+  // Instantiates a client
+  const storage = Storage();
+
+  // References an existing bucket, e.g. "my-bucket"
+  const bucket = storage.bucket(facialanalysisbucket);
+
+  // Uploads a local file to the bucket, e.g. "./local/path/to/file.txt"
+  return bucket.upload(test1.png)
+    .then((results) => {
+      const file = results[0];
+
+      console.log(`File ${file.name} uploaded.`);
+
+      return file;
+    });
+}
