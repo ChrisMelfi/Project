79808403cf3f1373ea9738d76ed2fd7fec356eef
ChrisMelfi <chris.melfi@hotmail.co.uk>
2017-04-19 02:57:11 +0100

    update

--- controllers/node_cloud_test.js
+++ controllers/node_cloud_test.js
@@ -41,13 +41,14 @@ exports.upload = function(req, res) {
   .then((results) => {
     const faces = results[0];
 
-    console.log('Faces:');
+    // console.log('Faces:');
     faces.forEach((face, i) => {
       console.log(`  Face #${i + 1}:`);
       console.log(`    Joy: ${face.joy}`);
       console.log(`    Anger: ${face.anger}`);
       console.log(`    Sorrow: ${face.sorrow}`);
       console.log(`    Surprise: ${face.surprise}`);
+      console.log(`Joy: ${face.joy}`, `Anger: ${face.anger}`, `Sorrow: ${face.sorrow}`, `Surprise: ${face.surprise}`);
     });
   });
 };
